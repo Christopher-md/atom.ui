28984e6bccfa8b7b1d9866243d12e4c29628d036
Christopher-md <tchaikovsky.md@gmail.com>
2023-04-26 09:11:11 +0300

    [feature] Remove zustand from the project

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -1,25 +1,20 @@
 /**
  * Created by chaikovskiy on 25.04.2023
  */
-import React from "react";
-import { shallow } from "zustand/shallow";
-import useCounter from "@/store/useCounter";
+import React, { useState } from "react";
 import Button from "@/components/UI/Button";
 import Typography from "@/components/UI/Typography";
 import styles from "./Counter.module.sass";
 
 const Counter: React.FC = () => {
-    const { count, increment, decrement } = useCounter(
-        (state) => ({
-            count: state.count,
-            increment: state.increment,
-            decrement: state.decrement,
-        }),
-        shallow,
-    );
+    const [count, setCount] = useState(0);
+
+    const increment = () => setCount((prev) => prev + 1);
+
+    const decrement = () => setCount((prev) => prev - 1);
 
     return (
-        <div>
+        <div className={styles.counter}>
             <Button onClick={decrement}>-</Button>
             <Typography className={styles.count}>
                 {count}
