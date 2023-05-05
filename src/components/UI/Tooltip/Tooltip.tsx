797ba66d333211de523013ba854622883276bf6d
Christopher-md <tchaikovsky.md@gmail.com>
2023-05-05 09:01:01 +0300

    [feature] Render a tooltip in the body

--- src/components/UI/Tooltip/Tooltip.tsx
+++ src/components/UI/Tooltip/Tooltip.tsx
@@ -1,17 +1,19 @@
-import React, { useRef, useState } from "react";
+import React, { useState } from "react";
 import classNames from "classnames";
 import { usePopper } from "react-popper";
+import { createPortal } from "react-dom";
 import { Transition } from "react-transition-group";
 import type Props from "./types";
 import styles from "./Tooltip.module.sass";
 
+const timeout = { appear: 0, exit: 500, enter: 500 };
+
 const Tooltip: React.FC<Props> = (props) => {
     const { text, children } = props;
 
-    const ref = useRef<HTMLDivElement | null>(null);
     const [isOpen, setIsOpen] = useState<boolean>(false);
     const [popper, setPopper] = useState<HTMLDivElement | null>(null);
-    const [tooltip, setTooltip] = useState<HTMLButtonElement | null>(null);
+    const [tooltip, setTooltip] = useState<HTMLDivElement | null>(null);
 
     const { styles: style, attributes } = usePopper(tooltip, popper, {
         modifiers: [{ name: "offset", options: { offset: [0, 8] } }],
@@ -22,11 +24,8 @@ const Tooltip: React.FC<Props> = (props) => {
     const handleMouseLeave = () => setIsOpen(false);
 
     return (
-        <div
-            ref={ref}
-            className={styles.wrapper}
-        >
-            <button
+        <>
+            <div
                 ref={setTooltip}
                 className={styles.tooltip}
                 aria-describedby="tooltip"
@@ -34,39 +33,38 @@ const Tooltip: React.FC<Props> = (props) => {
                 onMouseLeave={handleMouseLeave}
             >
                 {children}
-            </button>
+            </div>
 
-            <Transition
-                in={isOpen}
-                mountOnEnter
-                unmountOnExit
-                timeout={{
-                    appear: 0,
-                    exit: 500,
-                    enter: 500,
-                }}
-            >
-                {(state) => (
-                    <div
-                        role="tooltip"
-                        ref={setPopper}
-                        style={style.popper}
-                        className={classNames(
-                            styles.popper,
-                            styles[`animation-${state}`],
-                        )}
-                        {...attributes.popper}
-                    >
-                        {text}
+            {createPortal(
+                <Transition
+                    in={isOpen}
+                    mountOnEnter
+                    unmountOnExit
+                    timeout={timeout}
+                >
+                    {(state) => (
                         <div
-                            data-popper-arrow
-                            style={style.arrow}
-                            className={styles.arrow}
-                        />
-                    </div>
-                )}
-            </Transition>
-        </div>
+                            role="tooltip"
+                            ref={setPopper}
+                            style={style.popper}
+                            className={classNames(
+                                styles.popper,
+                                styles[`animation-${state}`],
+                            )}
+                            {...attributes.popper}
+                        >
+                            {text}
+                            <div
+                                data-popper-arrow
+                                style={style.arrow}
+                                className={styles.arrow}
+                            />
+                        </div>
+                    )}
+                </Transition>,
+                document.body,
+            )}
+        </>
     );
 };
 
